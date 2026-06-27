58ea22436edc1b5b013437e96d8a0ed5bf5afa0a
vikas012-code <vikasthapa115@gmail.com>
2026-06-27 16:14:26 +0530

    Update fetch URL to include '/wake' endpoint

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/wake",(req ,res)=>{
 
 setTimeout(() => {
         console.log("after 60 seconds");
-        fetch("https://service-runner.onrender.com")
+        fetch("https://service-runner.onrender.com/wake")
         .then((res) => console.log("res-> ",res.statusText))
         .catch((err)=> console.log("catch error ",{err}))
     }, 60000);
